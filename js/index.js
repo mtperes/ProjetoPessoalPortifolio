7ef0c40ef2a4b98cd32baa813355cf950bb93695
eugenioduarte <eugenioduartedasilva@yahoo.com.br>
2017-01-29 03:47:30 +0000

    Primeiro Commit
    01

---
+++ js/index.js
@@ -0,0 +1,27 @@
+//GreenSocks efects
+window.onload = function() {
+  		var logo = document.getEle("hello");
+  		TweenLite.from(logo, 2, {top:"1500px"});
+  		TweenLite.to(logo, 2, {down:"380px"});
+		}
+
+(document).ready(function(){
+	$("span").click(function({
+		$("img").attr("src",I"img/alvo.png);-→ira altera o attr src da tag img para o alvo.
+	}))
+})
+$(":button").click(function(){
+	$(":radio").css("border", "3px solid red")
+}) 
+
+<div class="container">
+  <h2>Modal Example</h2>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+  
+  
+</div>
+
+</body>
+</html>
